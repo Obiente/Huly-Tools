3f34dc3bfec3b47c091cbb509f8841232972ca85
veryCrunchy <me@verycrunchy.dev>
2025-07-19 20:26:46 +0200

    feat: update astro configuration and add vue integration

--- apps/admin/astro.config.mjs
+++ apps/admin/astro.config.mjs
@@ -1,11 +1,16 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
+import vue from "@astrojs/vue";
 
 // https://astro.build/config
 export default defineConfig({
+  output: "static",
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
+    optimizeDeps: {
+      include: ['vue']
+    }
   },
+  integrations: [vue()],
 });
